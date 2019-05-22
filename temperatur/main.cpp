fadce3425baf42ee43ad48536ed42b1cad9806d6
Ihr Name <ihre@emailadresse.de>
2019-05-22 13:53:53 +0200

    Umstellung auf Strukturen

--- temperatur/main.cpp
+++ temperatur/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <array>
 
 float tempf(float temp){
   float ergebnis;
@@ -17,28 +18,38 @@ float tempc(float temp){
 
 int main(int argc, char** argv){
 //Einlesen der Datei
+    struct Temperaturen{
+      std::array<float,4> wert;
+      std::array<char,4> einheit;
+    };
+    Temperaturen eingabe;
     std::ifstream in("input");
     std::ofstream out("output");
-    float wert;
-    char einheit;
     for (int i = 0; i < 4; ++i){
-        in>>wert;
-	in>>einheit;
-	if (einheit=='F'){
-	    wert=tempc(wert);
-	    einheit='C';
+        in>>eingabe.wert[i];
+	in>>eingabe.einheit[i];
+    }
+//Ausgabematrix definieren
+    Temperaturen ausgabe;
+//Ausrechnen
+	for (int i = 0; i < 4; ++i){  
+	if (eingabe.einheit[i]=='F'){
+	    ausgabe.wert[i]=tempc(eingabe.wert[i]);
+	    ausgabe.einheit[i]='C';
 	}
-	else if (einheit=='C'){
-	    wert=tempf(wert);
-	    einheit='F';
+	else if (eingabe.einheit[i]=='C'){
+	    ausgabe.wert[i]=tempf(eingabe.wert[i]);
+	    ausgabe.einheit[i]='F';
 	}
 	else{
 	std::cout << "Falsche Einheit in Zeile ";
-	std::cout << i << std::endl;
+        std::cout << i << std::endl;
 	}
-	out<<wert;
-	out<<" ";
-	out<<einheit<<std::endl;
+    }
+//Ausgabe
+    for (int i = 0; i < 4; ++i){
+      out << ausgabe.wert[i] << " ";
+      out << ausgabe.einheit[i] << std::endl;
     }
     std::cout << "Datei 'output' geschrieben!\n";
     return 0;
