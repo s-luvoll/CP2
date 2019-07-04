cb2bc48d8a8b452319856f1078e4407d96d48feb
Ihr Name <ihre@emailadresse.de>
2019-07-04 14:12:49 +0200

    ineratialer commit

---
+++ Projektarbeit/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+//global variables
+float hight=10;
+float steps=1;
+
+int monkey(){
+    float v_1=0;
+    int i=1;
+    std::ofstream out ("Monkey.txt");
+
+    //mainloop
+    while(hight>0){
+	float v_2=v_1+1/6.0*steps*K(v_1);
+    }
+
+}
+
+int main(int argc, char** argv){
+    
+    return 0;
+}
